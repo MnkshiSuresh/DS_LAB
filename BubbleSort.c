257ef91091d37c7e10d33629cadbe1e5c42e9833
Meenakshi Suresh <119519427+MnkshiSuresh@users.noreply.github.com>
2023-01-05 08:41:07 +0530

    Update BubbleSort.c

--- BubbleSort.c
+++ BubbleSort.c
@@ -1,25 +1,27 @@
 #include<stdio.h>
 int main(){
-    int array[50],n,i,j,temp,exchange=1;
-    printf("enter the no of elements");
+    int n,i,exchange=1;
+    printf("enter the no of elements:");
     scanf("%d",&n);
-    printf("enter the numbers:");
+    int a[n];
+    printf("enter the elements:");
     for(i=0;i<n;i++){
-        scanf("%d",&array[i]);
-    }
-    for(i=0;i< (n-1)&&exchange;i++){
-        for(j=0,exchange=0;j<n-i-1;j++){
-            if(array[j]>array[j+1]){
-                temp=array[j];
-                array[j]=array[j+i];
-                array[j+1]=temp;
-                exchange=1;
+    scanf("%d",&a[i]);}
+    
+    for(i=0;i<(n-1)&&exchange;i++){
+        for(int j=0,exchange=0;j<n-i-1;j++){
+            int temp;
+            if(a[j]>a[j+1]){
+            temp=a[j];
+            a[j]=a[j+1];
+            a[j+1]=temp;
+            exchange=1;
             }
         }
     }
-    printf("the sorted array is:");
+    printf("Sorted list:");
     for(i=0;i<n;i++){
-        printf("%d",array[i]);
+        printf("\n%d",a[i]);
     }
     return 0;
 }
